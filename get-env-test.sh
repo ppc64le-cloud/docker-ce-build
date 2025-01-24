1ec85ab83dd39a94e52039a40631248bc6bdf0ea
Ashwin Hendre <ashwin.hendre2@ibm.com>
2025-01-24 16:02:45 +0530

    Set FILE_ENV

--- get-env-test.sh
+++ get-env-test.sh
@@ -2,6 +2,7 @@
 # Script to get containerd repository if RUNC_VERSION is unspecified
 
 set -o allexport
+FILE_ENV="env.list"
 source /workspace/${FILE_ENV}
 if [[ -z ${CONTAINERD_RUNC_TAG} ]]; then    
     git clone --depth 1 --branch ${CONTAINERD_TAG} https://github.com/containerd/containerd.git
