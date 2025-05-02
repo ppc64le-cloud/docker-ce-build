4203f4c16f2957f0c5cf5fb8251cb44887012886
Ashwin Hendre <ashwin.hendre2@ibm.com>
2025-05-02 19:18:58 +0530

    Scripts to use 1.0-ppc64le DinD image
    Update scripts to use 1.0-ppc64le DinD image. The GO_VERSION used for tests is obtained from the official Go website. The latest DinD and dockerd-entrypoint script hashes are obtained from their corresponding Docker repositories. The Docker and containerd versions are obtained from the latest versions published on download.docker.com

--- test.sh
+++ test.sh
@@ -130,27 +130,19 @@ BUILD_ARGS+=" --build-arg GO_VERSION=${GO_VERSION}"
 
   echo "### # Building the test image: ${IMAGE_NAME} # ###"
   # Building the test image
-  if [[ "${DISTRO_NAME}:${DISTRO_VERS}" == centos:8 ]]; then
+  if [[ "${DISTRO_NAME}:${DISTRO_VERS}" == centos:9 ]]; then
     ##
-    # Switch to quay.io for CentOS 8 stream
-    # See https://github.com/docker/containerd-packaging/pull/263
-    # See https://github.com/docker-library/official-images/pull/11831
-    ##
-    echo "Temporary fix: patching Dockerfile for using CentOS 8 stream and quay.io "
-    sed -i 's/FROM ppc64le.*/FROM quay.io\/centos\/centos\:stream8/g' Dockerfile
-  elif [[ "${DISTRO_NAME}:${DISTRO_VERS}" == centos:9 ]]; then
-    ##
-    # Switch to quay.io for CentOS 8 stream
+    # Switch to quay.io for CentOS 9 stream
     # See https://github.com/docker/containerd-packaging/pull/283
     ##
     echo "Temporary fix: patching Dockerfile for using CentOS 9 stream and quay.io "
-    sed -i 's/FROM ppc64le.*/FROM quay.io\/centos\/centos\:stream9/g' Dockerfile
+    sed -i 's/FROM public.ecr.aws.*/FROM quay.io\/centos\/centos\:stream9/g' Dockerfile
   elif [[ "${DISTRO_NAME}:${DISTRO_VERS}" == centos:10 ]]; then
     ##
     # Switch to quay.io for CentOS 10 stream
     ##
     echo "Temporary fix: patching Dockerfile for using CentOS 10 stream and quay.io "
-    sed -i 's/FROM ppc64le.*/FROM quay.io\/centos\/centos\:stream10/g' Dockerfile
+    sed -i 's/FROM public.ecr.aws.*/FROM quay.io\/centos\/centos\:stream10/g' Dockerfile
   fi
  
   BUILD_ARGS+=" --build-arg DISTRO_NAME=${DISTRO_NAME} --build-arg DISTRO_VERS=${DISTRO_VERS}"
